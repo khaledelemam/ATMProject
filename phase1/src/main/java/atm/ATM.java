e9aabd55a416fae023087880737e313ffcf0e425
Amanda Leiva <amandaleiva@outlook.com>
2019-02-20 18:38:07 -0500

    test

---
+++ phase1/src/main/java/atm/ATM.java
@@ -0,0 +1,21 @@
+package atm;
+
+import java.util.Scanner;
+
+public class ATM {
+
+    private Scanner reader = new Scanner(System.in);
+
+
+    private void login() {
+        System.out.println("1. Login \n 2. Create new user");
+        String option = reader.nextLine();
+    }
+
+    public static void main(String[] args) {
+
+
+
+
+    }
+}
