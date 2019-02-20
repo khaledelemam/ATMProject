3ae3528b5e3a2e9c7468efb95ad77def60edc9dd
Amanda Leiva <amandaleiva@outlook.com>
2019-02-20 18:58:48 -0500

    began working on the ATM class

--- phase1/src/main/java/atm/ATM.java
+++ phase1/src/main/java/atm/ATM.java
@@ -4,18 +4,43 @@ import java.util.Scanner;
 
 public class ATM {
 
-    private Scanner reader = new Scanner(System.in);
+    private static Scanner reader = new Scanner(System.in);
 
 
-    private void login() {
-        System.out.println("1. Login \n 2. Create new user");
-        String option = reader.nextLine();
+    private static boolean login() {
+        System.out.println("Enter username: ");
+        String username = reader.nextLine();
+        System.out.println("Enter password: ");
+        String password = reader.nextLine();
+
+        // if username is valid
+            // if password matches username
+                // return true
+            // else re-prompt for username and password
+        return true;
+    }
+    private static void newUser() {
+        System.out.println("Enter username: ");
+        String username = reader.nextLine();
+        // call to BankManager to create a new user account with default password
+        // cannot have two users with the same username
+    }
+
+    private static void menu() {
+
+    }
+
+    private static void accounts() {
+
     }
 
     public static void main(String[] args) {
-
-
-
-
+        System.out.println("1. Login \n2. Create new user");
+        int option = Integer.parseInt(reader.nextLine());
+        if (option == 1) {
+            login();
+        } else if (option == 2) {
+            newUser();
+        }
     }
 }
