d3a77d4ce333a390308fcb52f146b12deece1529
Amanda Leiva <amandaleiva@outlook.com>
2019-02-21 18:37:36 -0500

    added the constructor and getters for chequing account

--- phase1/src/main/java/atm/ChequingAccount.java
+++ phase1/src/main/java/atm/ChequingAccount.java
@@ -1,4 +1,37 @@
 package atm;
 
 public class ChequingAccount {
+
+    private User owner;
+    private int balance;
+    private Transaction lastTransaction;
+    private Date dateOpened;
+
+    public ChequingAccount(User owner) {
+        this.owner = owner;
+        this.balance = 0;
+        this.lastTransaction = null;
+//        this.dateOpened = getCurrentDate
+    }
+
+    public int getBalance() {
+        return this.balance;
+    }
+
+    public User getOwner() {
+        return this.owner;
+    }
+
+    public Date getDateOpened() {
+        return dateOpened;
+    }
+
+    public Transaction getLastTransaction() {
+        return this.lastTransaction;
+    }
+
+    public void setLastTransaction(Transaction newTransaction) {
+        this.lastTransaction = newTransaction;
+    }
+
 }
