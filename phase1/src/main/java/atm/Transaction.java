2b47d047ee06694932cd595ee94701e508dae369
Amanda Leiva <amandaleiva@outlook.com>
2019-02-21 03:21:32 -0500

    created the transaction object class + added relevant getters and settes :^)

--- phase1/src/main/java/atm/Transaction.java
+++ phase1/src/main/java/atm/Transaction.java
@@ -1,4 +1,44 @@
 package atm;
 
 public class Transaction {
+
+    private User sender;
+    private User recipient;
+    private int amount;
+    private Date date;
+
+    public Transaction(User sender, User recipient, int amount) {
+        this.sender = sender;
+        this.recipient = recipient;
+        this.amount = amount;
+//        this.date = ...
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public User getRecipient() {
+        return recipient;
+    }
+
+    public User getSender() {
+        return sender;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+
+    // these will be used when the bank manager reverses a transaction
+
+    public void setSender(User sender) {
+        this.sender = this.recipient;
+
+    }
+
+    public void setRecipient(User recipient) {
+        this.recipient = recipient;
+    }
 }
