c1024cf86b9d8a127e9ac22330c9da2188e31e11
khaledelemam <khaled.elemam@mail.utoronto.ca>
2019-03-02 14:59:39 -0500

    test

--- phase1/src/main/java/atm/User.java
+++ phase1/src/main/java/atm/User.java
@@ -10,17 +10,21 @@ public class User implements Serializable {
     private String username;
     private String password;
     private int numAccounts;
-    private ArrayList<Account> accounts;
-    private ChequingAccount primaryAccount;
+    private HashMap<Integer,Account> accounts = new HashMap<>();
+    private HashMap<Account,Integer> map = new HashMap<>();
+    private Account request;
 
 
     public User(String username) {
         this.numAccounts = 0;
         this.username = username;
-        this.accounts = new ArrayList<>();
+//        this.accounts = new ArrayList<>();
 
         ChequingAccount firstAccount = new ChequingAccount(this);
-        this.accounts.add(firstAccount);
+
+        this.accounts.put(1, firstAccount);
+        this.map.put(firstAccount, 1);
+
     }
 
 
@@ -32,12 +36,28 @@ public class User implements Serializable {
 
     public String getPassword() {return this.password;}
 
-    public ChequingAccount getPrimaryAccount() { return this.primaryAccount; }
-// testing git
+
+    public void addAccount(){
+        SavingsAccount s = new SavingsAccount((this));
+        this.accounts.put(2, s);
+        this.map.put(s, 2);
+
+    }
+
+    public void viewAccounts(){
+        for(int i = 1 ; i <=accounts.size(); i++){
+            String str = Integer.toString(map.get(accounts.get(i)));
+            System.out.println("(" +str + ")" +accounts.get(i));
+
+        }
 
 
+    }
 
+    public void requestAccount(Account account){
+        request = account;
+    }
 
-
+    //testgit
 
 }
