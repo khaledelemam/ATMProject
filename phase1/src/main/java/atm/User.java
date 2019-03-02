ba8175562d2c0b3a71da517cb54fd76e65c19714
khaledelemam <khaled.elemam@mail.utoronto.ca>
2019-03-02 16:50:33 -0500

    User

--- phase1/src/main/java/atm/User.java
+++ phase1/src/main/java/atm/User.java
@@ -12,18 +12,34 @@ public class User implements Serializable {
     private int numAccounts;
     private HashMap<Integer,Account> accounts = new HashMap<>();
     private HashMap<Account,Integer> map = new HashMap<>();
-    private Account request;
+    private Account request = null;
+
+    // I used two Hashmaps to track the account with the corresponding number. Two are used instead of one because there is no "getKey()" in Hashmaps.
+    // This implementation is working so far in accessing user accounts.
+    // I did not use arrayList because I could not track the accounts as users can add different accounts in different orders
 
 
     public User(String username) {
         this.numAccounts = 0;
         this.username = username;
-//        this.accounts = new ArrayList<>();
 
-        ChequingAccount firstAccount = new ChequingAccount(this);
 
-        this.accounts.put(1, firstAccount);
-        this.map.put(firstAccount, 1);
+
+
+        //There should not be a "default" account, the user requests the specific account
+        // these accounts are just for testing purposes.
+
+        SavingsAccount s = new SavingsAccount((this));
+        this.accounts.put(1, s);
+        this.map.put(s, 1);
+
+
+        ChequingAccount c = new ChequingAccount(this);
+        this.accounts.put(2, c);
+        this.map.put(c, 2);
+
+
+
 
     }
 
@@ -37,12 +53,14 @@ public class User implements Serializable {
     public String getPassword() {return this.password;}
 
 
-    public void addAccount(){
+    public void addAccount(Account account){
         SavingsAccount s = new SavingsAccount((this));
         this.accounts.put(2, s);
         this.map.put(s, 2);
 
     }
+
+
 
     public void viewAccounts(){
         for(int i = 1 ; i <=accounts.size(); i++){
@@ -65,9 +83,6 @@ public class User implements Serializable {
 
     }
 
-    public void requestAccount(Account account){
-        request = account;
-    }
 
     public void viewBalance (){
         for(int i = 1 ; i <=accounts.size(); i++){
@@ -77,6 +92,15 @@ public class User implements Serializable {
         }
 
 
+    }
+
+
+    public Account getRequest(){
+        return request ;
+    }
+
+    public void requestAccount(Account account){
+        request = account;
     }
 
 }
