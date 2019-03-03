65ed794f1dffb0d4bae9ceb5ab5b4ebb868d0b23
khaledelemam <khaled.elemam@mail.utoronto.ca>
2019-03-03 02:42:54 -0500

    minor fix

--- phase1/src/main/java/atm/CreditCard.java
+++ phase1/src/main/java/atm/CreditCard.java
@@ -1,10 +1,18 @@
 package atm;
 
+import java.text.DecimalFormat;
+
 public class CreditCard extends Account {
     private int balance;
+    private DecimalFormat currencyFormat = new DecimalFormat("0.00");
 
     public CreditCard(User owner) {
         super(owner);
+
+    }
+    @Override
+    public String getBalance() {
+        return currencyFormat.format(this.balance);
     }
 
     public void setBalance(double amount) {
