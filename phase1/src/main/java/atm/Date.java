9896f4c1792293f4599f7a9b554e0bce28e6d87b
doroudi5 <bahareh.doroudiani@mail.utoronto.ca>
2019-03-06 13:24:11 -0500

    Created Date constructor to construct current date, and created update() method to update date to tomorrow.

--- phase1/src/main/java/atm/Date.java
+++ phase1/src/main/java/atm/Date.java
@@ -1,21 +1,67 @@
 package atm;
 
 
+import java.io.*;
+import java.io.Serializable;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 
 /*Keep track of time in ATM.*/
-public class Date extends java.util.Date {
+public class Date extends java.util.Date implements Serializable {
 
     private int day;
     private int month;
     private int year;
+    private static final String filename = "date";
+
+    //    private DateFormat sdf;
     //private static int today; //???
+
+    public Date() {
+        try {
+            FileInputStream fis = new FileInputStream(filename);
+            ObjectInputStream ois = new ObjectInputStream(fis);
+            Date today = (Date) ois.readObject();
+            ois.close();
+            fis.close();
+        } catch (IOException ioe) {
+            ioe.printStackTrace();
+
+        } catch (ClassNotFoundException c) {
+            System.out.println("Class not found");
+            this.day = 1;
+            this.month = 1;
+            this.year = 2019;
+            System.out.println(this);
+            c.printStackTrace();
+        }
+    }
 
     public void setDate(int day, int month, int year) {
         this.month = month;
         this.day = day;
         this.year = year;
+    }
+
+    public void update() {
+        this.day++;
+        if (this.day == 31) {
+            this.day = 1;
+            this.month++;
+        }
+        if (this.month == 13) {
+            this.month = 1;
+            this.year++;
+        }
+        try {
+            FileOutputStream fos = new FileOutputStream(filename);
+            ObjectOutputStream oos = new ObjectOutputStream(fos);
+            oos.writeObject(this);
+            oos.close();
+            fos.close();
+        } catch (IOException ioe) {
+            ioe.printStackTrace();
+        }
     }
 
     @Override
