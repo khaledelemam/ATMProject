b6e820e627307d9788be3cf90f41b53d68fe32e8
doroudi5 <bahareh.doroudiani@mail.utoronto.ca>
2019-03-06 08:19:59 -0500

    Created Date setter and added toString method.

--- phase1/src/main/java/atm/Date.java
+++ phase1/src/main/java/atm/Date.java
@@ -5,25 +5,18 @@ import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 
 /*Keep track of time in ATM.*/
-public class Date extends java.util.Date{
+public class Date extends java.util.Date {
+
     private int day;
     private int month;
     private int year;
     //private static int today; //???
-
-	public Date() {
-
-    }
 
     public void setDate(int day, int month, int year) {
         this.month = month;
         this.day = day;
         this.year = year;
     }
-
-
-}
-
 
     @Override
     public String toString() {
