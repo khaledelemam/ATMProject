e8816aa760fb882d1f50e9df17413c7b2e884a5d
doroudi5 <bahareh.doroudiani@mail.utoronto.ca>
2019-03-05 23:36:01 -0500

    Added date setter

--- phase1/src/main/java/atm/Date.java
+++ phase1/src/main/java/atm/Date.java
@@ -1,4 +1,21 @@
 package atm;
 
+/*Keep track of time in ATM.*/
 public class Date {
+    private int day;
+    private int month;
+    private int year;
+    //private static int today; //???
+    
+	public Date() {
+
+    }
+
+    public void setDate(int day, int month, int year) {
+        this.month = month;
+        this.day = day;
+        this.year = year;
+    }
+
+
 }
