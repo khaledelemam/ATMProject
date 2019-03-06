fbd8a2b036b7d757d22efe5d79be9486dcae1bb7
Amanda Leiva <amandaleiva@outlook.com>
2019-03-05 23:09:02 -0500

    date

--- phase1/src/main/java/atm/Date.java
+++ phase1/src/main/java/atm/Date.java
@@ -1,4 +1,13 @@
 package atm;
 
-public class Date {
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+
+public class Date extends java.util.Date {
+
+    @Override
+    public String toString() {
+        DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+        return dateFormat.format(this);
+    }
 }
