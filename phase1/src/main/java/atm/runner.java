9f0938ded0b2335f5d7d7c8bb79df4d2942bac60
Wen Jie <wenjie.liu@mail.utoronto.ca>
2019-03-09 20:51:30 -0500

    added NegativeDenominationException to method signature

--- phase1/src/main/java/atm/runner.java
+++ phase1/src/main/java/atm/runner.java
@@ -9,7 +9,7 @@ public class runner {
 
 
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, NegativeDenominationException {
 
         Scanner in = new Scanner(System.in);
 
