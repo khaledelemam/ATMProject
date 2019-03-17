192854355f7feb4409ddb14b98210ad1f20911cf
doroudi5 <bahareh.doroudiani@mail.utoronto.ca>
2019-03-16 21:20:21 -0400

    DebtAccounts now throw excpetion when max debt limit is reached

--- phase2/src/main/java/atm/DebtAccounts.java
+++ phase2/src/main/java/atm/DebtAccounts.java
@@ -1,18 +1,21 @@
 package atm;
 
+/**
+ * Accounts that accumulate debt. Positive net total means user owes money.
+ */
 public class DebtAccounts extends Account {
     private double maxDebt = 500.;
 
     public DebtAccounts() {
         super();
     }
-
     @Override
-    public void setBalance(double amount) {
+    public void setBalance(double amount) throws InsufficientFundsException {
         if (this.balance - amount >= -maxDebt) {
             this.balance -= amount;
         } else {
-            System.out.println("You have reached the max debt.");
+            InsufficientFundsException e = new InsufficientFundsException("You have reached your debt limit.");
+            throw e;
         }
 
     }
