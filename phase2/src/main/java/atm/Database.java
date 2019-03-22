f72df7e5ce84c0d777b3b445dbc6265e41d8770a
khaledelemam <khaled.elemam@mail.utoronto.ca>
2019-03-21 20:56:13 -0400

    refactored database to get rid of static

--- phase2/src/main/java/atm/Database.java
+++ phase2/src/main/java/atm/Database.java
@@ -8,18 +8,18 @@ public class Database {
 
     //TODO: Try passing in the serializable stuff in Database constructor and storing each thing ( pass in transactions, users, accounts..)
 
-    private static ArrayList<User> users = new ArrayList<>();
+    static private ArrayList<User> users = new ArrayList<>();
 
-    static ArrayList<User> getUsers() {
+    ArrayList<User> getUsers() {
         return users;
     }
 
-    static void addUser(User user){
+    void addUser(User user){
         users.add(user);
         store();
     }
 
-    static User checkExistingUser(String username){
+    User checkExistingUser(String username){
         for (User user:users) {
             if (user.getUsername().equals(username)) {
                 return user;
@@ -28,7 +28,7 @@ public class Database {
         return null;
     }
 
-   static User login(String username, String password){
+    User login(String username, String password){
        retrieve();
        for (User user : users) {
            if (user.getUsername().equals(username)) {
@@ -42,7 +42,7 @@ public class Database {
     }
 
 
-    static  void  store(){
+      void  store(){
         try{
             FileOutputStream fos = new FileOutputStream("file");
             ObjectOutputStream oos = new ObjectOutputStream(fos);
@@ -58,14 +58,13 @@ public class Database {
 
     @SuppressWarnings("unchecked")
 
-   static  void retrieve() {
+    void retrieve() {
         File f = new File("file");
         if (f.exists()) {
             try {
                 FileInputStream fis = new FileInputStream("file");
                 ObjectInputStream ois = new ObjectInputStream(fis);
                 users = (ArrayList) ois.readObject();
-                System.out.println(users.get(0).getPassword());
                 ois.close();
                 fis.close();
             } catch (IOException ioe) {
